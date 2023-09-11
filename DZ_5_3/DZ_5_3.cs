6fbcf248d2635a151049acdf28193b874e41d0f6
pveom <pveomegq@gmail.com>
2023-09-11 18:09:57 +0300

    DZ_5

---
+++ DZ_5_3/DZ_5_3.cs
@@ -0,0 +1,28 @@
+﻿//4.перевірка чи є введене натуральне число простим
+
+
+Random random = new Random();
+int gennum = random.Next(2, 1000);
+
+
+
+ bool CheckNum(int i, bool indikator)
+{
+    
+    for (i = 2; i < gennum; i++)
+    {
+        if (gennum % i == 0)
+        {
+            return indikator = false;
+        }
+        else return indikator = true;
+    }
+    return indikator;
+ }
+
+if (CheckNum(2,false)) Console.WriteLine("not simple");
+else Console.WriteLine("simple"); 
+
+
+
+Console.ReadLine();
