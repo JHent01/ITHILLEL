e584013b2c6803ce7fa07082ad288208212f9734
pveom <pveomegq@gmail.com>
2023-09-15 18:54:39 +0300

    DZ_6

---
+++ DZ_6/DZ_6_4/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Random rand = new Random();
+
+int[] randNumbers = new int[20];
+
+for (int i = 0; i < randNumbers.Length; i++)
+{
+    randNumbers[i] = rand.Next(, 100);
+}
+
+
+
+PrintMass(randNumbers);
+
+void PrintMass(int[] longArray)
+{
+    foreach (int i in longArray)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+//Console.ReadKey();
+
+
+//for (int i = 0; i < randNumbers.Length; i++)
+//{
+//    randNumbers[int.Purse(ReadLine())] = 00    
+//}
+
+
---
+++ Dz_5-1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Write("Введіть пароль:");
+
+string chek = Console.ReadLine();
+string pass = "root";
+string ChekPass(string chek )
+{
+    
+
+    while (chek != pass)
+    {
+        Console.WriteLine($"password {chek} don`t wrong");
+    }
+
+    return chek;
+
+}
+
+
+
+
+
+
+
+
+Console.ReadLine();
